2b1a7c126d932dbb926d240caf5bdd6e912d76b8
Elliot Yap <yilliot@gmail.com>
2017-10-22 08:06:31 +0800

    key value 05

--- el/demo/05.php
+++ el/demo/05.php
@@ -13,17 +13,18 @@ do {
     if ($input_name !== 'x') {
 
         $input_age = readline('input age : ');
-        $new_person['name'] = $input_name;
-        $new_person['age'] = $input_age;
-        $array_of_ages[] = $new_person;
+        $array_of_ages[$input_name] = $input_age;
+        // $new_person['name'] = $input_name;
+        // $new_person['age'] = $input_age;
+        // $array_of_ages[] = $new_person;
     }
 
 } while($input_name !== 'x');
 
 echo  PHP_EOL . "++++++++++++++++++++++++++++++++++" . PHP_EOL . PHP_EOL;
 echo ' Name ' . "\t |" . ' Age' . PHP_EOL;
-foreach ($array_of_ages as $person) {
-    echo $person['name'] . " \t | " . $person['age'] . PHP_EOL;
+foreach ($array_of_ages as $name => $age) {
+    echo " $name \t | $age" . PHP_EOL;
 }
 
 
